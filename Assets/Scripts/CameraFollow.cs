e84c5fb9bbef234577f7f4c9f6ee4bf5f79fdbe8
1MochaChan1 <danny8doo@gmail.com>
2023-07-18 18:14:53 +0530

    init commit, victorian assets added

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,15 +7,15 @@ public class CameraFollow : MonoBehaviour
     public Transform targetTransform;
     public float followSpeed=2f;
 
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+  
     void Update()
     {
-        Vector3 newPos = new Vector3(targetTransform.position.x, transform.position.y, transform.position.z);
-        transform.position = Vector3.Slerp(transform.position, newPos, followSpeed*Time.deltaTime);
+        transform.position = new Vector3(transform.position.x+5f*Time.deltaTime, transform.position.y, -10f);
+        if(targetTransform != null)
+        {
+            Vector3 newPos = new Vector3(targetTransform.position.x, transform.position.y, transform.position.z);
+            transform.position = Vector3.Slerp(transform.position, newPos, followSpeed*Time.deltaTime);
+        }
     }
 }
