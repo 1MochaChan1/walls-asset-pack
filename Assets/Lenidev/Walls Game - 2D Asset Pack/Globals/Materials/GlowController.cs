7016c4c8407c57e1bf3b1fd8fdedd8db10ae9902
1MochaChan1 <danny8doo@gmail.com>
2023-07-27 23:03:33 +0530

    Futuristic assets added, draft for asset store created.

---
+++ Assets/Lenidev/Walls Game - 2D Asset Pack/Globals/Materials/GlowController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class GlowController : MonoBehaviour
+{
+    public new SpriteRenderer renderer;
+    [ColorUsage(true, true)]
+    public Color glowColor;
+    //public Texture2D mainTexture;
+    public Texture2D glowMaskTexture;
+
+    private void Start()
+    {
+        if(renderer != null)
+        {
+            renderer.material.SetColor("_GlowColor", glowColor);
+            renderer.material.SetTexture("_MainText", renderer.sprite.texture);
+            renderer.material.SetTexture("_GlowMaskTex", glowMaskTexture);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //renderer.material.SetColor("_GlowColor", glowColor);
+        //renderer.material.SetTexture("_MainText", renderer.sprite.texture);
+        //renderer.material.SetTexture("_GlowMaskTex", glowMaskTexture);
+    }
+}
