22d67d6f9d10bb3e3d62c97f2f4059d1b378d0eb
1MochaChan1 <danny8doo@gmail.com>
2023-07-31 23:54:59 +0530

    forest trunk added

--- Assets/Lenidev/Walls Game - 2D Asset Pack/Globals/Scripts/Parallax.cs
+++ Assets/Lenidev/Walls Game - 2D Asset Pack/Globals/Scripts/Parallax.cs
@@ -19,29 +19,32 @@ public class Parallax : MonoBehaviour
     void Update()
     {
 
-        // Checking how much distance is covered by the camera
-        float distCovered = (cam.transform.position.x * (1-parallaxEffect));
-
-
-        // Distance the sprite will move along with the camera.
-        float distToMove = (cam.transform.position.x * parallaxEffect);
-
-        // Moving the sprite along the x-axis on the by 'dist' units.
-        transform.position= new Vector3(distToMove+startPos, transform.position.y, transform.position.z);
-
-
-
-        if (distCovered > startPos + length)
+        if (cam!=null)
         {
-            // If distance covered by camera is more than starting position of the sprite,
-            // then the sprite will be shifted by 'length' units
-            startPos += length;
-        }
-        else if (distCovered < startPos - length)
-        {
-            // If distance covered by camera is less than starting position of the sprite,
-            // then the sprite will be shifted by 'length' units
-            startPos -= length;
+            // Checking how much distance is covered by the camera
+            float distCovered = (cam.transform.position.x * (1-parallaxEffect));
+
+
+            // Distance the sprite will move along with the camera.
+            float distToMove = (cam.transform.position.x * parallaxEffect);
+
+            // Moving the sprite along the x-axis on the by 'dist' units.
+            transform.position= new Vector3(distToMove+startPos, transform.position.y, transform.position.z);
+
+
+
+            if (distCovered > startPos + length)
+            {
+                // If distance covered by camera is more than starting position of the sprite,
+                // then the sprite will be shifted by 'length' units
+                startPos += length;
+            }
+            else if (distCovered < startPos - length)
+            {
+                // If distance covered by camera is less than starting position of the sprite,
+                // then the sprite will be shifted by 'length' units
+                startPos -= length;
+            }
         }
     }
 }
